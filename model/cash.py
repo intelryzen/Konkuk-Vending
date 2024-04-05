a5805a035bea4570305ef1607d4694fade1d301e
우승식 <intelryzen@naver.com>
2024-04-06 02:05:02 +0900

    fix: 코드 이동

--- model/cash.py
+++ model/cash.py
@@ -1,27 +1,7 @@
-import re
 
-Currency_List = [] #권종 클래스 리스트 생성
-
-def Save_Currencies(cashFilePath, Currency): #잔돈 파일 생성/저장
-	"""
-	저장하는 과정에서 저장에 대한 에러는 무시하는지 정해진 것으로 아는데 맞는지 확인 필요함
-	"""
-	with open(cashFilePath, 'w') as file:
-		for Currency in Currency_List:
-			file.write(f"{Currency.value} {Currency.quantity}\n") #공백으로 권종, 개수 분리
-
-def Load_Currencies(cashFilePath, Currency): #잔돈 파일 로드
-	"""
-	잔돈 파일에서 100원, 500원, 1000원, 5000원, 10000원, 50000원 외에 
-	다른 권종 있는지 로드하면서 판별하는 과정 필요 -> 무결점 검사에서 추가바람
-
-	또한 권종 개수 이후 다른 문자에 대한 예외 처리 혹은 에러 처리가 필요함
-	"""
-	with open(cashFilePath, 'r') as file:
-		for line in file:
-			parts = re.split(r'\s+', line.strip()) #횡공백류열1 기준으로 분리
-			try:
-				value, quantity = parts[0], parts[1]
-			except IndexError:
-				pass
-			Currency_List.append(Currency(int(value), int(quantity))) #인스턴스 생성 (리스트)
+class Currency: #권종 클래스
+	def __init__(self, value, quantity):
+			self.value = value #권종
+			self.quantity = quantity #개수
+	def __str__(self):
+		return f"{self.value} {self.quantity}"
