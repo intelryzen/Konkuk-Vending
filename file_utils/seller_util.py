0055fe5f11d5b6bf9b185c30a74748f4fb01cfdf
우승식 <intelryzen@naver.com>
2024-04-06 02:11:00 +0900

    fix: 코드 이동

---
+++ file_utils/seller_util.py
@@ -0,0 +1,22 @@
+import re
+
+def Load_Admin(sellerFilePath, Admin, Admin_List): #관리자 파일 로드
+	"""
+	아이디 비밀번호 이후 다른 문자에 대한 예외 처리 혹은 에러 처리가 필요함
+	"""
+	with open(sellerFilePath, 'r') as file:
+		for line in file:
+			parts = re.split(r'\s+', line.strip()) #횡공백류열1 기준으로 분리
+			try:
+				name, password = parts[0], parts[1]
+			except IndexError:
+				pass
+			Admin_List.append(Admin(name, password)) #인스턴스 생성 (리스트)
+
+def Save_Admin(sellerFilePath, Admin, Admin_List): #관리자 파일 생성/저장
+	"""
+	저장하는 과정에서 저장에 대한 에러는 무시하는것으로 하는것이 맞는지 확인 필요함
+	"""
+	with open(sellerFilePath, 'w') as file:
+		for Admin in Admin_List:
+			file.write(f"{Admin.name} {Admin.password}\n") #공백으로 아이디, 비밀번호 분리
