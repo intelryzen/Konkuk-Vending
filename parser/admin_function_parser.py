2af234ea1a945d3e335f8a34868e7a5ca9bcb3e6
minus7896 <80439030+minus7896@users.noreply.github.com>
2024-04-09 12:26:49 +0900

    Update admin_function_parser.py

--- parser/admin_function_parser.py
+++ parser/admin_function_parser.py
@@ -19,8 +19,12 @@ from base_parser import BaseParser
     해당 음료수 번호는 이미 선점되었습니다. (음료수 추가) -> 오류: 음료수의 가격은 1000000원 이하이어야 합니다. 완
     음료수의 가격은 100의 배수이어야 합니다. (음료수 추가) 완
     음료수 개수 입력시 0과 99사이의 숫자만 기입해 주세요. (음료수 추가) 완
+    명령어 0에 해당하는 인자 개수 확인 완
 '''
 
+
+#음료수 번호와 이름 리스트를 어떤 타입으로 받아올지 -> 파일 담당하시는 분과 상의
+# 1번 메뉴와 2번 메뉴 정상 수행 결과, 반환 값 설정 -> 프롬프트 담당하는 분과 상의
 
 
 
@@ -29,10 +33,10 @@ class AdminFunctionParser(BaseParser):
         
         #파일 이름 앞뒤 공백 제거
         file_name = drinkfilename.strip()
-        drinknum = []  #음료 번호 리스트
+        drinknum = []  #음료 번호 리스트      #인자로 받아와야함
         drinkname = [] #음료 이름 리스트
 
-        #파일에 있는 음료 번호, 이름 불러오기
+        #파일에 있는 음료 번호, 이름 불러오기 > 음료 번호 리스트와 음료 이름 리스트로 받아오기
         with open(file_name, 'r', encoding='utf-8') as file:
             for line in file:
                 # 앞뒤 공백 제거
@@ -63,9 +67,9 @@ class AdminFunctionParser(BaseParser):
             if len(parts) > 1:
                 return False, "오류 : 인자가 없어야 합니다."
             if command == 1:
-                 return True,"권종별 수량을 비싼 권종부터 하나씩 출력" # 이부분 어떻게 하면 좋을까요?
+                 return True,"권종별 수량을 비싼 권종부터 하나씩 출력"  #프롬프트 하시는 분들에게 물어봐서 편한 방식으로 수정
             if command == 2:
-                 return True,"권종별 수량을 비싼 권종부터 하나씩 출력" # 이부분 어떻게 하면 좋을까요?
+                 return True,"한 행에 음료수의 번호, 이름, 가격, 개수를 출력"
             
         # 3번 4번 메뉴
         elif command in [3, 4]:
@@ -127,6 +131,8 @@ class AdminFunctionParser(BaseParser):
         
         # 0 입력
         elif command == 0:
+            if len(command)!=1 :
+                return False, "오류: 인자가 없어야 합니다."
             return True, ""
         
  
