ed476f92bbae5628e3f02f2f1a53a11cf9342d7f
bashdas <125852968+bashdas@users.noreply.github.com>
2024-04-10 17:09:19 +0900

    Add files via upload

---
+++ prompt/cash_in.py
@@ -0,0 +1,7 @@
+class CashIn:
+
+    def __init__(self):
+        print("금액을 투입해주세요.")
+        cash_in = input(">>>")
+        # cash_in 입력 값이 올바른 지 검사
+        
---
+++ prompt/drink_in.py
@@ -0,0 +1,7 @@
+class DrinkIn:
+
+    def __init__(self):
+        print("구매하실 음료수 번호를 입력해주세요.")
+        drink_in = input(">>>")
+        # drink_in 입력 값이 올바른 지 검사
+        
