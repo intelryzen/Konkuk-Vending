c859afda86a8575d492e4f65eb75352cde85b910
bashdas <125852968+bashdas@users.noreply.github.com>
2024-04-11 01:50:07 +0900

    Update drink_selection.py

--- prompt/drink_selection.py
+++ prompt/drink_selection.py
@@ -9,14 +9,27 @@ class DrinkSelection:
     def __init__(self):
         self.drink_selection_prompt()
 
+
     # 음료수 선택 프롬프트
     def drink_selection_prompt(self):
         command = input("\n구매하실 음료수 번호를 입력해주세요.(0. 거스름 돈 반환 및 뒤로가기)\n>>>")
-        # 0: 거스름돈 출력하고 음료수 목록 출력 후 음료수 선택 프롬프트로 이동 
-        #    잔액 0 아니면 음료수 선택 프롬프트로 이동, 잔액 0원이면 금액 입력 프롬프트로 이동
+
+        if DrinkSelectionParser.parse(command) == [true, 0]:
+        # 0: 거스름돈 출력하고 음료수 목록 출력 후 금액입력 프롬프트로 이동
+        Change(CashInputParser.parse(command)[1])
+        CashInput()
+
+        elif DrinkSelectionParser.parse(command) == [true, int]:
         # 정상: 잔액 업데이트, 음료수 목록 업데이트 후 출력, 
+            if change_remain == 0:
+                drink_selection_prompt()
+        #    잔액 0 아니면 음료수 선택 프롬프트로 이동, 잔액 0원이면 금액 입력 프롬프트로 이동
         #       음료수 재고 0이면 drinks.txt에서 삭제. 음료수 선택 프롬프트로 이동
+        DrinkSelection()
+
+        else:
         # 비정상: 음료수 선택 프롬프트로 이동
+        self.drink_selection_prompt()
 
     # 잔액 업데이트
     
