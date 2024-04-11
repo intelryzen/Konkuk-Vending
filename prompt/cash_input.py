1b613af9a795c517f639a29b29a4b3a270294f21
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-04-11 22:20:42 +0900

    Update cash_input.py

--- prompt/cash_input.py
+++ prompt/cash_input.py
@@ -1,3 +1,5 @@
+from parser.cash_input_parser import CashInputParser
+
 class CashInput:
     '''
     금액 입력 프롬프트
@@ -10,15 +12,20 @@ class CashInput:
     def __init__(self):
         self.cash_input_prompt()
 
+    def cash_input_prompt(self):
+        print("금액을 입력해주세요")
+        command = input(">>>")
+        
         parser = CashInputParser()
         parsed_command = parser.parse(command)
-
+        
         if parsed_command == (True, 0):
-            # 입력 0: 모드 선택 프롬프트로 복귀    
-            Mode()
+            from .mode import Mode
+            Mode()  
+            
 
         elif parsed_command[0] and isinstance(parsed_command[1], dict):
-            # 정상: 음료수 선택 프롬프트로 이동
+            from .drink_selection import DrinkSelection
             DrinkSelection()
 
         else:
@@ -28,5 +35,5 @@ class CashInput:
             
 
 if __name__ == "__main__":
-    # 금액 입력 프롬프트 테스트
+    # 임의의 값을 전달하는 대신, cash_input_prompt 메서드 내에서 입력을 받도록 설정합니다.
     cashinput = CashInput()
