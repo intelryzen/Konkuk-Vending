3262dee5e582b500603304c9ff1805380a4697ca
consommes <nudago476@gmail.com>
2024-04-11 15:47:00 +0900

    feat: nvm

--- file_utils/seller_util.py
+++ file_utils/seller_util.py
@@ -4,7 +4,7 @@ import re
 아이디 비밀번호 변경 함수 추가할지 논의 필요함
 """
 class Seller_Utils:
-	def load_admin(seller_file_path, admin_list, Admin): #관리자 파일 로드
+	def load_admin(self, seller_file_path, admin_list, Admin): #관리자 파일 로드
 		"""
 		아이디 비밀번호 이후 다른 문자에 대한 예외 처리 혹은 에러 처리가 필요함
 		"""
@@ -25,7 +25,7 @@ class Seller_Utils:
 			print("관리자 로그인 정보 파일 내 데이터가 없습니다. 프로그램을 종료합니다.")
 			exit()
 
-	def save_admin(seller_file_path, admin_list, Admin): #관리자 파일 생성/저장
+	def save_admin(self, seller_file_path, admin_list, Admin): #관리자 파일 생성/저장
 		"""
 		저장하는 과정에서 저장에 대한 에러는 무시하는것으로 하는것이 맞는지 확인 필요함
 		"""
