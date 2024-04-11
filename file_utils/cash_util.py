2079a5df242ac0e49ab157a0ff77fcaa676846bb
consommes <nudago476@gmail.com>
2024-04-11 15:46:26 +0900

    feat : error message added

--- file_utils/cash_util.py
+++ file_utils/cash_util.py
@@ -1,9 +1,10 @@
 import re
 import parser
 from config import config as c
+import parser.base_parser
 
 class Cash_Utils:
-	def save_currencies(cash_file_path, Currency): #잔돈 파일 생성/저장
+	def save_currencies(self,cash_file_path, Currency): #잔돈 파일 생성/저장
 		"""
 		저장하는 과정에서 저장에 대한 에러는 무시하는지 정해진 것으로 아는데 맞는지 확인 필요함
 		"""
@@ -11,7 +12,7 @@ class Cash_Utils:
 			for Currency in c.currency_list:
 				file.write(f"{Currency.value} {Currency.quantity}\n") #공백으로 권종, 개수 분리
 
-	def load_currencies(cash_file_path, Currency): #잔돈 파일 로드
+	def load_currencies(self,cash_file_path, Currency): #잔돈 파일 로드
 		"""
 		잔돈 파일에서 100원, 500원, 1000원, 5000원, 10000원, 50000원 외에 
 		다른 권종 있는지 로드하면서 판별하는 과정 필요 -> 무결점 검사에서 추가바람
@@ -26,23 +27,28 @@ class Cash_Utils:
 			Currency(10000,0),
 			Currency(50000,0)
 		]
-		with open(cash_file_path, 'r') as file:
-			for line in file:
-				parts = re.split(r'\s+', line.strip()) #횡공백류열1 기준으로 분리
-				if parser.base_parser.BaseParser.is_money_type(parser.base_parser.BaseParser, parts[0]):
-					for Currency in c.currency_list:
-						if str(Currency.value) == str(parts[0]):
-							Currency.quantity += int(parts[1])
-							if parser.base_parser.BaseParser.is_count(parser.base_parser.BaseParser, str(Currency.quantity)):
-								pass
-							else:
-								print("count error message")
-								exit()
-				else:
-					print("currency error message")
-					exit()
+		try:
+			with open(cash_file_path, 'r') as file:
+				for line in file:
+					parts = re.split(r'\s+', line.strip()) #횡공백류열1 기준으로 분리
+					if parser.base_parser.BaseParser.is_money_type(parser.base_parser.BaseParser, parts[0]):
+						for Currency in c.currency_list:
+							if str(Currency.value) == str(parts[0]):
+								Currency.quantity += int(parts[1])
+								if parser.base_parser.BaseParser.is_count(parser.base_parser.BaseParser, str(Currency.quantity)):
+									pass
+								else:
+									print("파일 내 <개수>를 확인하십시오.")
+									exit()
+					else:
+						print("허용되지 않은 권종이 포함된 파일입니다. 파일 내 권종을 확인하십시오.")
+						exit()
+		except FileNotFoundError:
+			print("잔돈 파일이 없습니다. 파일을 생성합니다.")
+			self.save_currencies(Cash_Utils, cash_file_path, Currency)
+			exit()
 
-	def change_currency(Currency, Currency_Value, Currency_Amount):
+	def change_currency(self, Currency, Currency_Value, Currency_Amount):
 		for Currency in c.currency_list:
 			if Currency.value == Currency_Value:
 				if parser.base_parser.BaseParser.is_count(parser.base_parser.BaseParser,str(Currency.quantity +Currency_Amount)):
--- file_utils/seller_util.py
+++ file_utils/seller_util.py
@@ -8,14 +8,23 @@ class Seller_Utils:
 		"""
 		아이디 비밀번호 이후 다른 문자에 대한 예외 처리 혹은 에러 처리가 필요함
 		"""
-		with open(seller_file_path, 'r') as file:
-			for line in file:
-				parts = re.split(r'\s+', line.strip()) #횡공백류열1 기준으로 분리
-				try:
-					name, password = parts[0], parts[1]
-				except IndexError:
-					pass
-				admin_list.append(Admin(name, password)) #인스턴스 생성 (리스트)
+		try:
+			with open(seller_file_path, 'r') as file:
+				for line in file:
+					parts = re.split(r'\s+', line.strip()) #횡공백류열1 기준으로 분리
+					try:
+						name, password = parts[0], parts[1]
+					except IndexError:
+							pass
+					admin_list.append(Admin(name, password)) #인스턴스 생성 (리스트)
+		except FileNotFoundError:
+			print("관리자 로그인 정보 파일이 없습니다. 프로그램을 종료합니다.")
+			exit()
+
+		if not admin_list:
+			print("관리자 로그인 정보 파일 내 데이터가 없습니다. 프로그램을 종료합니다.")
+			exit()
+
 	def save_admin(seller_file_path, admin_list, Admin): #관리자 파일 생성/저장
 		"""
 		저장하는 과정에서 저장에 대한 에러는 무시하는것으로 하는것이 맞는지 확인 필요함
