bbaa8e01f58d5674252247e237e3882be5e91e07
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-04-11 18:31:38 +0900

    Update login.py

--- prompt/login.py
+++ prompt/login.py
@@ -16,20 +16,25 @@ class Login:
 
     # 관리자 로그인 프롬프트
     def show_admin_login_prompt(self):
-        print("\n<관리자 로그인>")
-        print("이전 화면으로 가려면 '0'을 입력해주세요.")
-        print("-------------------------------------------")
-    
-        login_input = input("로그인\n>>>")
+        while True: # 잘못된 입력이면 로그인 프롬프트 반복
+            print("\n<관리자 로그인>")
+            print("이전 화면으로 가려면 '0'을 입력해주세요.")
+            print("-------------------------------------------")
+        
+            login_input = input("로그인\n>>>")
 
-        is_valid, result = self.parser.parse(login_input)  # parse 메서드 호출 시 입력 문자열 전달
+            is_valid, result = self.parser.parse(login_input)  # parse 메서드 호출 시 입력 문자열 전달
 
-        if is_valid:
-            print("로그인 성공")
-            admin = Admin()  # 로그인 성공 시 관리자 프롬프트로 이동
-        else:
-            print(result)  # 오류 메시지 출력
-            adminlogin = Login() # 오류 메시지 출력한 후 관리자 로그인 프롬프트로 이동
+            if is_valid:
+                if result == 0:
+                    Mode() # 모드 선택 프롬프트로 이동
+
+                else:
+                    print("로그인 성공")
+                    Admin()  # 로그인 성공 시 관리자 프롬프트로 이동
+            else:
+                print(result)  # 오류 메시지 출력
+                continue
 
 # 로그인 프롬프트 테스트
-adminlogin = Login()
+Login()
