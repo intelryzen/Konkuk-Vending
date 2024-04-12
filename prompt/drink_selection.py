c4ca5b45e497703bb01e8daed0fb4a2b81e6ecfc
minus7896 <80439030+minus7896@users.noreply.github.com>
2024-04-12 16:12:32 +0900

    메인 음료수 구매까지 수행 됩니다

--- prompt/drink_selection.py
+++ prompt/drink_selection.py
@@ -15,46 +15,61 @@ class DrinkSelection:
     '''
     
     def __init__(self):
-        self.drink_selection_prompt()
         self.drink_list = ShowDrinksList() #음료수 목록 출력 
-        self.command = input("\n구매하실 음료수 번호를 입력해주세요.(0. 거스름 돈 반환 및 뒤로가기)\n>>>")
+       
 
     # 음료수 선택 프롬프트
     def drink_selection_prompt(self):
-
+        self.command = input("\n구매하실 음료수 번호를 입력해주세요.(0. 거스름 돈 반환 및 뒤로가기)\n>>>")
         parser = DrinkSelectionParser()
-        parsed_command = parser.parse(self.command)
-
-
+        drink_check, parsed_command = parser.parse(self.command)
+        print(drink_check)
+        print(parsed_command)
+        drink_price=0
+        
         # 0: 거스름돈 출력하고 음료수 목록 출력 후 금액입력 프롬프트로 이동
-        if  parsed_command == 0:
-            canChange = Change(c.cash_by_cus) # 거스름 돈 출력
+        if  drink_check == True:
+            if parsed_command ==0:
+                canChange = Change(c.cash_by_cus) # 거스름 돈 출력
+                c.cash_by_cus =0
+                return
+            else :
+                # 구매자가 선택한 음료의 가격을 drink_price 변수로 저장
+                for i in range (0, len(c.drinks_list)):
+                    if int(c.drinks_list[i].number) == int(parsed_command):
+                        drink_price = c.drinks_list[i].price
+                r_flag = self.remain_cash(c.cash_by_cus, drink_price,parsed_command)
+                if r_flag==False:
+                    return
+                else:
+                    return self.drink_selection_prompt()
+            '''
             if not canChange:
                 c.cash_by_cus += drink_price
-            return CashInput() # 금액입력 프롬프트로 이동
+            '''
+        else:
+            print(parsed_command)       
+            return self.drink_selection_prompt()
 
         # 정상: 잔액과 업데이트 된 음료수 목록 출력 후 음료수 선택 프롬프트로 이동
-        elif parsed_command == 1:
+        
 
-            # 구매자가 선택한 음료의 가격을 drink_price 변수로 저장
-            for i in range (0, len(c.drink_list), 4):
-                if c.drinks_list[i] == int(self.command):
-                    drink_price = c.drink_list[i+2]
-
-            self.remain_cash(c.cash_by_cus, drink_price)
-
-    def remain_cash(self, cash_by_cus, drink_price):
-        if cash_by_cus <= drink_price: # (잔액 혹은 투입금액) < (음료수 가격)인 경우
+    def remain_cash(self, cash_by_cus, drink_price,number):
+        print(cash_by_cus)
+        print(drink_price)
+        print(number)
+        if cash_by_cus < drink_price: # (잔액 혹은 투입금액) < (음료수 가격)인 경우
             print("오류: 금액이 부족합니다. 다른 음료수를 선택해주세요")
-            return CashInput() # 잔액 0원이면 금액 입력 프롬프트로 이동 
-            print("잔액:",c.cash_by_cus,"원") # 해당 위치에서 잔액 출력
-
+            return True
         else:
             c.cash_by_cus = c.cash_by_cus - drink_price
-            Drinks_util.buy_drink(self.command) # 해당 위치에서 음료수 목록 파일 업데이트
-            print("잔액:",c.cash_by_cus,"원") # 해당 위치에서 잔액 출력
+            Drinks_util().buy_drink(str(number)) # 해당 위치에서 음료수 목록 파일 업데이트
             self.drink_list.show_drinks_list() # 음료수 목록 출력 
-            return self.drink_selection_prompt() #음료수 선택 프롬프트로 이동
+            print("잔돈 :" ,c.cash_by_cus)
+            if c.cash_by_cus ==0:
+                return False #잔돈이 0일 때
+            
+            return True
 
 
 if __name__ == "__main__":
