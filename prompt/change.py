7586c6559b06dd973d89f58a1828e75dc9c88783
slee1148 <135320739+slee1148@users.noreply.github.com>
2024-04-14 15:31:56 +0900

    Update change.py

--- prompt/change.py
+++ prompt/change.py
@@ -1,10 +1,31 @@
 from config import config as c
 
-def Change(change):
+def Change(drink):
+	change = 0
+	for i in range(6):
+		change += (c.customer_list[i].value * c.customer_list[i].quantity)
+		c.currency_list[i].quantity += c.customer_list[i].quantity
 	won = [0] * 6
+	change -= drink
+	ret = change
 	i = 5
 	while change > 0:
-		if change > c.currency_list[i].value and c.currency_list[i].quantity > 0:
+		if change >= c.currency_list[i].value and c.currency_list[i].quantity > 99:
+			change -= c.currency_list[i].value
+			c.currency_list[i].quantity -= 1
+			won[i] += 1
+		if change < c.currency_list[i].value:
+			if c.currency_list[i].quantity < 100:
+				i -= 1
+			else:
+				for j in range(6):
+					c.currency_list[j].quantity += won[j]
+					c.currency_list[j].quantity -= c.customer_list[j].quantity
+				return False, "오류: 잔돈이 포화상태입니다. 관리자에게 문의하거나 타 음료수를 선택해주세요." 
+		if i == -1:
+			break
+	while change > 0:
+		if change >= c.currency_list[i].value and c.currency_list[i].quantity > 0:
 			change -= c.currency_list[i].value
 			c.currency_list[i].quantity -= 1
 			won[i] += 1
@@ -12,9 +33,9 @@ def Change(change):
 			i -= 1
 		if i < 0:
 			for j in range(6):
-				c.currency_list[i].quantity += won[j]
-			print("잔돈이 부족합니다.")
-			return False
+				c.currency_list[j].quantity += won[j]
+				c.currency_list[j].quantity -= c.customer_list[j].quantity
+			return False, "오류: 잔돈이 부족합니다."
 
 	if ret == 0:
 		return True, "거스름돈: 0원"
