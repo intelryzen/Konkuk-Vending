8ba3ee4edafe632515e28e9882f188765a64ceaa
Minju <angertiger0941@gmail.com>
2024-04-26 20:46:52 +0900

    fix: noFile

--- file_utils/cash_util.py
+++ file_utils/cash_util.py
@@ -58,7 +58,7 @@ class Cash_Utils(BaseParser):
 						sys.exit()
 		except FileNotFoundError:
 			print("잔돈 파일이 없습니다. 파일을 생성합니다.")
-			self.save_currencies(Cash_Utils, cash_file_path, Currency)
+			self.save_currencies(cash_file_path, Currency)
 		if self.is_all_quantity_zero(c.currency_list, Currency):
 			print("잔돈 파일 내 데이터가 없습니다.")
 
