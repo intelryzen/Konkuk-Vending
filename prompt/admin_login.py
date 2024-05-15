c811893ad8058cd48f7e995d51deeca25d18af48
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-15 15:08:12 +0900

    Add files via upload
    내부 함수명 변경 AdminLogin

---
+++ prompt/admin_login.py
@@ -0,0 +1,43 @@
+from my_parser.admin_login_parser import AdminLoginParser
+from config import config as c
+
+class Login:
+    '''
+        관리자 로그인 프롬프트
+        command:
+            0: 모드 선택 프롬프트로 이동
+            그외: 
+                비정상: 오류 메시지 + 관리자 로그인 프롬프트로 이동
+                정상: "로그인 성공" 관리자 프롬프트로 이동
+    '''    
+    def __init__(self):
+        self.parser = AdminLoginParser()
+        
+    # 관리자 로그인 프롬프트
+    def show_admin_login_prompt(self):
+        print("\n<관리자 로그인>")
+        print("이전 화면으로 가려면 '0'을 입력해주세요.")
+        print("-------------------------------------------")
+        while True:
+            login_input = input("로그인\n>>>")
+
+            for seller in c.admin_list:
+                is_valid, result = self.parser.parse(login_input, seller.name, seller.password)  # parse 메서드 호출 시 입력 문자열 전달
+
+            if is_valid:
+                if result == 0:
+                    return 0
+                else:
+                    print("로그인 성공")
+                    return 1
+                    '''
+                    from .admin_function import Admin
+                    Admin()  # 로그인 성공 시 관리자 프롬프트로 이동
+                    '''
+            else:
+                print(result)  # 오류 메시지 출력
+                continue
+
+if __name__ == "__main__":
+    # 로그인 프롬프트 테스트
+    Login()
