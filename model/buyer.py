4bf4ee4b85c9925ef22e9b0ce7f0e7e17ab0ebfa
우승식 <intelryzen@naver.com>
2024-05-21 21:10:34 +0900

    feat: model.buyer

---
+++ model/buyer.py
@@ -0,0 +1,5 @@
+class Buyer:
+    def __init__(self, buyer_id:str, money:int, coupon_number:int):
+        self.buyer_id = buyer_id
+        self.money = money
+        self.coupon_number = coupon_number
