9ea6cd9640086274affa96cda8742b4a83564342
bashdas <125852968+bashdas@users.noreply.github.com>
2024-05-22 15:42:52 +0900

    Update drinksInfo_util.py

--- file_utils/drinksInfo_util.py
+++ file_utils/drinksInfo_util.py
@@ -4,13 +4,13 @@ import sys
 from config import Config as c
 from my_parser.base_parser import BaseParser
 from model.drink_info import Drink_info
-from file_utils.slot_util import SlotUtils
 
 class DrinkInfoUtils(BaseParser):
     def read(self):
         try:
             with open(c.DRINKS_FILE_PATH, 'r', encoding='utf-8') as file:
                 lines = file.readlines()
+                drink_nums = set()
 
                 for line in lines:
                     data = line.split()
@@ -31,13 +31,13 @@ class DrinkInfoUtils(BaseParser):
                     if not super().is_word(input=data[1]):  
                         raise ValueError(f"음료수 이름 (문법)오류가 확인되었습니다. {comment}")
                       
-                    if(data[2]<100 or data[2]>1000000 or data[2]%100 != 0): #base parser에 있는 is_price 쓰면 될거 같은데
+                    if not super().is_price(input=data[2]):  
                         raise ValueError(f"음료수 가격 오류가 확인되었습니다. {comment}")
-                
-                    if any(drink_num.drink_number == int(data[0]) for drink_num in c.drinks_list): # 
+                   
+                    if any(drink.drink_number == int(data[0]) for drink_num in c.drinks_list): 
                         raise ValueError(f"음료수 번호의 중복이 확인되었습니다. {comment}")
-            
-                c.drinks_list.append(Drink_info(drink_number=int(data[0]), drink_name=str(data[1]), price=int(data[2]))) 
+                   
+                    c.drink_list.append(Drink(drink_number=int(data[0]), drink_name=int(data[1]), price=int(data[2]))) 
 
                 if len(c.drinks_list) == 0:
                     print("경고: 자판기 슬롯 파일 내 데이터가 없습니다.")
@@ -50,8 +50,6 @@ class DrinkInfoUtils(BaseParser):
             print(f"오류: {e}")
             sys.exit()
            
-
-
     def __read_records(self):
         with open(c.DRINKS_FILE_PATH, 'r', encoding='utf-8') as file:
             lines = file.readlines()
@@ -61,21 +59,20 @@ class DrinkInfoUtils(BaseParser):
                 if line.strip(): 
                     records.append(record)
             return records
+
     def __write_records(self, records):
         with open(c.SLOTS_FILE_PATH, 'w') as file:
             file.writelines([f"{record[0]} {record[1]} {record[2]}\n" for record in records])
 
-        self.__write_records(records)
-
     def delete_drink(self, drink_number:int):
         records = self.__read_records()
         records = [record for record in records if int(record[0]) != drink_number]
-        if(drink_number in c.slots_list[1]):
+        if(drink_number in c.slots_list[0]):
             SlotUtils.delete_slots_used_same_drink(drink_number)
         self.__write_records(records)
 
     def update_new_drinks(self, drink_num:int, drink_name:str, price:int):
         records = self.__read_records()
-        new_drink = (drink_num, drink_name, price)
+        new_drink = [str(drink_num), drink_name, str(price)] 
         records.append(new_drink)
         self.__write_records(records)
