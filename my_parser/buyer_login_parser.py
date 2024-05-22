c50531650e22567c3e8a1367def5fb800d2b8454
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-22 12:17:40 +0900

    feat: buyer_id랑 입력이 일치 하지 않을 때 추가하는 기능

--- my_parser/buyer_login_parser.py
+++ my_parser/buyer_login_parser.py
@@ -1,4 +1,7 @@
 from .base_parser import BaseParser
+from config import config as c
+from model.buyer import Buyer
+from file_utils.buyer_util import BuyerUtils
 
 '''
 구매자 로그인(2차 확장)##
@@ -24,11 +27,16 @@ class BuyerLoginParser(BaseParser):
             return False, "오류: 올바른 입력이 아닙니다."
 
         if len(input) == 1 and len(input[0]) <= 10 and self.is_word(input[0]):
-            if input[0] == id:
-                return True, None
-            else:
-                # 파일 업데이트 file.util id 생성
-                return True, None
+            for buyer in c.buyer_list:
+                if input[0] == buyer.buyer_id:
+                    return True, "로그인 성공"
+                else:
+                    buyer_utils = BuyerUtils()
+                    success = buyer_utils.insert_buyer(input[0])
+                    if success:
+                        return True, f"{input[0]} 아이디를 생성하였습니다."
+                    else:
+                        return False, "아이디 생성을 실패했습니다."
 
         return False, "오류: 아이디가 입력규칙을 준수하지 않습니다."
 
