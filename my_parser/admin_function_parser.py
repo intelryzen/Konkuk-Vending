6d21a35dbf47f53a1997cde2bd98b12facfa19f3
minus7896 <80439030+minus7896@users.noreply.github.com>
2024-05-26 14:46:36 +0900

    주석으로 작성했던 부분 바꾸고, 오류가 생겼던 문구 부분 수정했습니다.

--- my_parser/admin_function_parser.py
+++ my_parser/admin_function_parser.py
@@ -3,34 +3,26 @@ from config import config as c
 
 
 
-allows = [0,1,2,3,4,5] ##0~7 까지 명령어
+allows = [0,1,2,3,4,5,6,7] ##0~7 까지 명령어
 class AdminFunctionParser(BaseParser):
     def parser(self, input: str ) -> tuple[bool,any]:  
         
         
         drinknum = []  #음료 번호 리스트    
-        drinkname = [] #음료 이름 리스트  >> 필요 없을 수도 있음
-        #drinkprice = [] ##음료 가격 리스트 추가 >>필요 없을 수도 있음
-        '''
         s_num = [] ## 슬롯 번호 리스트
         s_drink_num = [] ##슬롯에 있는 음료 번호 리스트
-        s_drink_count =[] ## 슬롯에 있는 음료 재고 리스트 >> 필요 없을 수도 있음
-        '''
+        
         
 
         #drinklist에서 drinknum , drinkname 분리하기
         for i in range(len(c.drinks_list)):
-                drinknum.append(c.drinks_list[i].drink_number)  # .append()를 사용하여 값을 추가
-                drinkname.append(c.drinks_list[i].name)  # .append()를 사용하여 값을 추가
-                #dirnkprice.append(c.drinks_list[i].price)
+                drinknum.append(c.drinks_list[i].number)  # .append()를 사용하여 값을 추가
         
         #슬롯의 정보 가져오기
-        '''
-        for i in range(len(c.slot_list)):
-                s_num.append(c.slot_list[i].number) 
-                s_drink_num.append(c.slot_list[i].drinknum)  
-                s_drink_count.append(c.slot_list[i].count)
-        '''
+        for i in range(len(c.slots_list)):
+                s_num.append(c.slots_list[i].number)
+                s_drink_num.append(c.slots_list[i].drinknum)  
+        
 
         # 입력 문자열 앞뒤 공백 제거
         input = input.strip()
@@ -76,38 +68,13 @@ class AdminFunctionParser(BaseParser):
             if command == 4:
                 if not self.is_number(parts[1]):
                     return False , "오류 : 음료수의 슬롯 번호는 숫자만 입력할 수 있습니다."
-                if not(parts[1] in drinknum) : ##if not(parts[1] in s_num) :
+                if not(parts[1] in s_num) :
                      return False, "오류: 존재하지 않는 음료수 슬롯 번호입니다. 사용하는 슬롯만 재고를 수정할 수 있습니다."
                 if not self.is_count(parts[2]):
                     return False, "오류: 음료수의 개수의 입력은 오직 0과 99사이의 숫자만 허용합니다."
                 return True,command
               
 
-        
-        # 5번 메뉴
-        elif command == 5:
-            # 5번 메뉴 인자 개수 확인
-            if len(parts) != 5:
-                return False, "오류: 4개의 인자가 필요합니다. 첫번째 인자는 번호, 두번째 인자는 이름, 세번째 인자는 가격, 네번째 인자는 개수를 입력해주세요.(음료수 추가)"
-            
-            # 5번 음료수 추가 명령어인자 유효성 검사
-
-            if not self.is_number(parts[1]):
-                return False, "오류: 음료수의 번호는 1과 99사이의 숫자만 허용합니다."
-            if parts[1] in drinknum :
-                return False, "오류: 해당 음료수 번호는 중복되어서 사용할 수 없습니다."
-            if not parts[3].isdecimal() :
-                return False, "오류: 음료수의 가격 입력시 숫자만 입력해 주세요."
-            price = int(parts[3])
-            if price<100: 
-                return False, "오류: 음료수의 가격은 최소 100원이어야 합니다."
-            if price>1000000: 
-                return False, "오류: 음료수의 가격은 1000000원 이하이어야 합니다."
-            if price%100 != 0: 
-                return False, "오류: 음료수의 가격은 100의 배수이어야 합니다."
-            if not self.is_count(parts[4]):
-                return False, "오류: 음료수 개수 입력시 0과 99사이의 숫자만 기입해 주세요."
-            return True,command
 
         # 0 입력
         elif command == 0:
@@ -115,16 +82,14 @@ class AdminFunctionParser(BaseParser):
                 return False, "오류: 인자가 없어야 합니다."
             return True,command# ""
         
-
-        '''
         # 5번 6번 메뉴
         elif command in [5,6]:
             # 5,6번 메뉴 인자 개수 확인
             if len(parts) != 4:
                 if command == 5:
-                    return False, “오류: 3개의 인자가 필요합니다. 첫번째 인자는 슬롯 번호, 두번째 인자는 음료수 번호, 세번째 인자는 개수를 입력해주세요.”
+                    return False, "오류: 3개의 인자가 필요합니다. 첫번째 인자는 슬롯 번호, 두번째 인자는 음료수 번호, 세번째 인자는 개수를 입력해주세요."
                 elif command == 6:
-                    return False,“오류: 3개의 인자가 필요합니다. 첫번째 인자는 음료수 번호, 두번째 인자는 이름, 세번째 인자는 가격을 입력해주세요.”
+                    return False,"오류: 3개의 인자가 필요합니다. 첫번째 인자는 음료수 번호, 두번째 인자는 이름, 세번째 인자는 가격을 입력해주세요."
             
             # 5번 자판기 슬롯에 음료수 할당 인자 유효성 검사
             if command == 5:
@@ -133,18 +98,18 @@ class AdminFunctionParser(BaseParser):
                 if not self.is_number(parts[2]):
                     return False, "오류: 음료수의 번호는 1과 99사이의 숫자만 허용합니다."
                 if not parts[2] in drinknum :   
-                    return False, “오류 : 해당 음료수 번호는 존재하지 않습니다.”
+                    return False, "오류 : 해당 음료수 번호는 존재하지 않습니다."
                 if parts[1] in s_num :
-                    return False, “오류: 해당 슬롯 번호는 중복되어서 사용할 수 없습니다.”
+                    return False, "오류: 해당 슬롯 번호는 중복되어서 사용할 수 없습니다."
                 if not self.is_count(parts[3]):
-                    return False, “오류: 음료수 개수 입력시 0과 99사이의 숫자만 기입해 주세요.”
+                    return False, "오류: 음료수 개수 입력시 0과 99사이의 숫자만 기입해 주세요."
                 return True,command
             # 6번  음료수 정보 추가 명령어 인자 유효성 검사
-             if command == 6:
+            if command == 6:
                 if not self.is_number(parts[1]):
-                    return False, “오류 : 음료수 번호는 1과 99사이의 숫자만 허용합니다.”
+                    return False, "오류 : 음료수 번호는 1과 99사이의 숫자만 허용합니다."
                 if parts[1] in drinknum :
-                    return False, “오류: 해당 음료수 번호는 중복되어서 사용할 수 없습니다.”
+                    return False, "오류: 해당 음료수 번호는 중복되어서 사용할 수 없습니다."
                 if not parts[3].isdecimal() :
                     return False, "오류: 음료수의 가격 입력시 숫자만 입력해 주세요."
                 price = int(parts[3])
@@ -157,18 +122,14 @@ class AdminFunctionParser(BaseParser):
                 if not self.is_count(parts[4]):
                     return False, "오류: 음료수 개수 입력시 0과 99사이의 숫자만 기입해 주세요."
                 return True,command
-        '''
-
-        '''
+          
         elif command == 7:
             # 1번 2번 메뉴 인자 개수 확인
             if not len(parts) == 2:
-                return False , “오류: 1개의 인자가 필요합니다. 인자는 음료수 번호를 입력해주세요.”
+                return False , "오류: 1개의 인자가 필요합니다. 인자는 음료수 번호를 입력해주세요."
             if not self.is_number(parts[1]):
-                return False, “오류 : 음료수 번호는 1과 99사이의 숫자만 허용합니다.”
+                return False, "오류 : 음료수 번호는 1과 99사이의 숫자만 허용합니다."
             if not parts[1] in drinknum :   
-                return False, “오류 : 해당 음료수 번호는 존재하지 않습니다.”
+                return False, "오류 : 해당 음료수 번호는 존재하지 않습니다."
             return True,command
-        '''
-        
  
