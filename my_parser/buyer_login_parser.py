12e81ca95fb0b1e824782200c6fe9c248cbe217c
GIRIBUIN <96866036+GIRIBUIN@users.noreply.github.com>
2024-05-27 23:39:02 +0900

    Update buyer_login_parser.py

--- my_parser/buyer_login_parser.py
+++ my_parser/buyer_login_parser.py
@@ -22,21 +22,31 @@ class BuyerLoginParser(BaseParser):
             return True, command
         
         input = self.parse_all(input)
-        
+
         if input == None or (len(input) == 1 and input[0] == ""):
             return False, "오류: 올바른 입력이 아닙니다."
 
         if len(input) == 1 and len(input[0]) <= 10 and self.is_word(input[0]):
+            found = False
             for buyer in c.buyer_list:
                 if input[0] == buyer.buyer_id:
+                    found = True
                     return True, "로그인 성공"
-            else:
+
+            if not found:
                 buyer_utils = BuyerUtils()
-                success = buyer_utils.insert_buyer(input[0])
-                if success:
-                    return True, f"{input[0]} 아이디를 생성하였습니다."
+                # 아이디가 이미 존재하는지 확인 후 존재하지 않을 때만 아이디 생성
+                if not any(buyer.buyer_id == input[0] for buyer in c.buyer_list):
+                    success = buyer_utils.insert_buyer(input[0])
+                    if success:
+                        # 새로운 아이디가 생성되면 c.buyer_list에도 추가
+                        new_buyer = Buyer(input[0],0,0)
+                        c.buyer_list.append(new_buyer)
+                        return True, f"{input[0]} 아이디를 생성하였습니다."
+                    else:
+                        return False, "아이디 생성을 실패했습니다."
                 else:
-                    return False, "아이디 생성을 실패했습니다."
+                    return False, "아이디가 이미 존재합니다."
 
         return False, "오류: 아이디가 입력규칙을 준수하지 않습니다."
 
